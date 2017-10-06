feb9dc8dc8d0896124016f6b1e373d49cb3ebdda
Miranda Jacobsen <mirjacobsen@gmail.com>
2017-10-06 15:31:13 -0700

    fix link in home navigation boxes

--- src/scripts/components/Home.js
+++ src/scripts/components/Home.js
@@ -17,11 +17,11 @@ class Home extends React.Component {
                         <img src="//api2.alexarauschernaturopath.com/api2/wp-content/uploads/2017/09/Detox-square.jpg" alt="Dr. Alexa Rauscher" className="nav-block__image"/>
                         <span className="nav-block__text">Dr. Alexa Rauscher</span>
                     </Link>
-                    <Link to='/naturopathic-philosophies' className="nav-block">
+                    <Link to='/about-naturopathic-medicine/naturopathic-philosophies' className="nav-block">
                         <img src="//api2.alexarauschernaturopath.com/api2/wp-content/uploads/2017/09/Supplements-square.jpg" alt="About Naturopathic Medicine"className="nav-block__image"/>
                          <span className="nav-block__text">Naturopathic Philosophies</span>
                     </Link>
-                    <Link to='/treatments' className="nav-block">
+                    <Link to='/conditions-and-treatments/treatments' className="nav-block">
                         <img src="//api2.alexarauschernaturopath.com/api2/wp-content/uploads/2017/09/homeopathy-square.jpg" alt="Conditions and Treatments"className="nav-block__image"/>
                         <span className="nav-block__text">Treatment Modalities</span>
                     </Link>
